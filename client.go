f72f7eb701b03f31c81b650de4e30f7f306de6bc
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-12-04 20:13:23 -0500

    initial commit

---
+++ client.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	client, _ := ethclient.Dial(os.Getenv("ETHEREUM_NODE"))
+
+	fmt.Println(client)
+	fmt.Println(os.Getenv("ETHEREUM_NODE"))
+
+}
