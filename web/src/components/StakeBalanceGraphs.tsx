3614a44c4acf415334aaea7d9b7e4268695c5de2
Samrat Sahoo <samratsahoo2013@gmail.com>
2024-01-28 16:28:32 -0500

    Add address to stake balance graphs

--- web/src/components/StakeBalanceGraphs.tsx
+++ web/src/components/StakeBalanceGraphs.tsx
@@ -11,6 +11,8 @@ export default function StakeBalanceGraphs({ dataPoints }: { dataPoints: LogEven
                         <div>
                             <div className="flex flex-row gap-2 items-center">
                                 <h1 className="text-black text-sm font-semibold">Stake Balances:</h1>
+                                <h1 className="truncate text-black text-sm">{data.length ? data[0].blockchainAddress : ""}</h1>
+
                             </div>
                             <LineChart data={data} dataKey="Lottery Balance" xLabel="Time" yLabel="Stake Balance"></LineChart> </div>
                     </div>)
